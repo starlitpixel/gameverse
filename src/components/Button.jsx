51ed7bb28a823d6d1b6cdfbcec7e3bfc3fae027b
Aiesha Ganguly <aieshaganguly@gmail.com>
2026-07-16 00:29:40 +0530

    build gameverse hero section

---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Button = ({ title, containerClass , leftIcon, rightIcon, id}) => {
+  return (
+    <button id={id} className={`group relative z-10 bg-violet-50 px-7 py-3 rounded-full ${containerClass}`}>
+      {leftIcon}
+
+      <span className='relative incline-flex overflow-hidden font-general text-xs uppercase'>
+        <div>{title}</div>
+      </span>
+      {rightIcon}
+
+    </button>
+  );
+};
+
+export default Button
